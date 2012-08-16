14b1c4507c7d5273ac6ebf92ad7551fa55b33fd1
Schuyler Erle <schuyler@nocat.net>
2012-08-16 11:32:43 -0700

    Make the grid API spit out GeoJSON, duh.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,7 +3,12 @@ require 'json'
 class ApiController < ApplicationController
   def grid
     @site = Site.find_by_name(params[:site])
-    @grid = Image.select("avg(points) as points, sum(hits) as hits, box", :group => "mgrs")
-    @grid.each {|g| g.box = JSON.parse(g.box) if g.box }
+    # TODO: filter by site
+    cells = Image.select("avg(points) as points, sum(hits) as hits, count(*) as images, box", :group => "mgrs")
+    features = cells.map {|cell|
+      box = cell.box ? JSON.parse(box) : []
+      {:type => "Feature", :properties => cell, :geometry => {:type => "Polygon", :coordinates => [box]}}
+    }
+    @grid = {:type => "FeatureCollection", :features => features}
   end
 end
