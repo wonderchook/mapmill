8bd009d0246b788a0f871c503559197368eafca1
Schuyler Erle <schuyler@nocat.net>
2012-11-21 03:02:40 +0000

    Add timestamps to hits.pl.

--- util/hits.pl
+++ util/hits.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl -n
 BEGIN {$/ = ""}
 my $ip = (m|((?:\d+\.){3}\d+)|gos)[0];
+my $stamp = (m|at (\d{4}-\d\d-\d\d \d\d:\d\d:\d\d)|gos)[0];
+my ($date, $time) = split " ", $stamp;
 my ($image, $score) = (m|/vote/(\d+)/(\d+)|gos);
-print join(",", $ip, $image, $score), "\n" if $ip and $image;
+print join(",", $date, $time, $ip, $image, $score), "\n" if $ip and $image;
