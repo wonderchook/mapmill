1e532250c58528892879bbeebb25c9326a8d58db
Schuyler Erle <schuyler@nocat.net>
2012-12-01 21:43:52 +0000

    New schema.rb after last migration.

--- db/schema.rb
+++ db/schema.rb
@@ -9,20 +9,105 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120816172807) do
+ActiveRecord::Schema.define(:version => 20121201125401) do
+
+  create_table "all_votes", :id => false, :force => true do |t|
+    t.string  "date",  :limit => nil
+    t.string  "time",  :limit => nil
+    t.string  "site",  :limit => nil
+    t.string  "email", :limit => nil
+    t.string  "ip",    :limit => nil
+    t.integer "image"
+    t.decimal "score"
+  end
+
+  add_index "all_votes", ["site"], :name => "all_votes_site"
+
+  create_table "giscorps_pilot_sample", :id => false, :force => true do |t|
+    t.string  "file",          :limit => nil
+    t.integer "sample_set"
+    t.integer "image"
+    t.integer "site_id"
+    t.string  "mgrs",          :limit => nil
+    t.integer "mapmill_image"
+  end
+
+  create_table "giscorps_pilot_votes", :id => false, :force => true do |t|
+    t.string  "date",         :limit => nil
+    t.string  "time",         :limit => nil
+    t.string  "site",         :limit => nil
+    t.string  "email",        :limit => nil
+    t.string  "ip",           :limit => nil
+    t.integer "image"
+    t.decimal "score"
+    t.string  "mgrs",         :limit => nil
+    t.string  "damage",       :limit => nil
+    t.string  "inundation",   :limit => nil
+    t.string  "damage_type",  :limit => nil
+    t.string  "damage_combo", :limit => nil
+    t.decimal "damage_value"
+  end
+
+  create_table "image_agreement", :id => false, :force => true do |t|
+    t.integer "id"
+    t.string  "date_",     :limit => nil
+    t.string  "time_",     :limit => nil
+    t.integer "votes"
+    t.float   "damage"
+    t.float   "agreement"
+  end
+
+  create_table "image_check_sample", :id => false, :force => true do |t|
+    t.integer "id"
+    t.string  "path", :limit => nil
+  end
+
+  create_table "image_diff", :id => false, :force => true do |t|
+    t.integer "id"
+    t.string  "mgrs"
+    t.decimal "mm_damage"
+    t.float   "ic_damage"
+    t.float   "delta"
+    t.integer "votes"
+  end
+
+  create_table "imagecat", :id => false, :force => true do |t|
+    t.string "usng",   :limit => nil
+    t.float  "damage"
+  end
+
+  create_table "imagecat2", :id => false, :force => true do |t|
+    t.string "usng",   :limit => nil
+    t.float  "damage"
+  end
+
+  add_index "imagecat2", ["usng"], :name => "imagecat2_usng", :unique => true
+
+  create_table "imagecat_damage", :id => false, :force => true do |t|
+    t.string "usng",         :limit => nil
+    t.string "damage",       :limit => nil
+    t.string "inundation",   :limit => nil
+    t.string "damage_type",  :limit => nil
+    t.string "damage_combo", :limit => nil
+  end
+
+  add_index "imagecat_damage", ["usng"], :name => "imagecat_damage_usng"
 
   create_table "images", :force => true do |t|
-    t.integer  "hits",       :default => 0,   :null => false
-    t.string   "filename",   :default => "",  :null => false
-    t.string   "path",       :default => "",  :null => false
-    t.integer  "points",     :default => 0,   :null => false
+    t.integer  "hits",        :default => 0,   :null => false
+    t.string   "filename",    :default => "",  :null => false
+    t.string   "path",        :default => "",  :null => false
+    t.integer  "points",      :default => 0,   :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "site_id"
-    t.decimal  "lat",        :default => 0.0
-    t.decimal  "lon",        :default => 0.0
-    t.string   "mgrs",       :default => ""
-    t.string   "box",        :default => ""
+    t.decimal  "lat",         :default => 0.0
+    t.decimal  "lon",         :default => 0.0
+    t.string   "mgrs",        :default => ""
+    t.string   "box",         :default => ""
+    t.string   "thumbnail",   :default => ""
+    t.string   "collection",  :default => ""
+    t.datetime "captured_at"
   end
 
   create_table "participants", :force => true do |t|
