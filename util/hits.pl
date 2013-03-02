0c4e6548866a88dd78cf0b3d45bc99d677c341d1
Ubuntu <ubuntu@mapmill.(none)>
2013-03-02 20:25:27 +0000

    Add signin page.

--- util/hits.pl
+++ util/hits.pl
@@ -1,7 +1,14 @@
 #!/usr/bin/perl -n
-BEGIN {$/ = ""}
+BEGIN {
+    $/ = "";
+    $filter = shift @ARGV;
+    print "date,time,site,email,ip,image,score\n";
+}
+next if $filter and not m|$filter/vote|os;
 my $ip = (m|((?:\d+\.){3}\d+)|gos)[0];
 my $stamp = (m|at (\d{4}-\d\d-\d\d \d\d:\d\d:\d\d)|gos)[0];
 my ($date, $time) = split " ", $stamp;
+my ($site) = (m|/review/(\w+)/vote|gos);
 my ($image, $score) = (m|/vote/(\d+)/(\d+)|gos);
-print join(",", $date, $time, $ip, $image, $score), "\n" if $ip and $image;
+my ($email) = m|Email: (\S+)|gos;
+print join(",", $date, $time, $site, $email, $ip, $image, $score), "\n" if $ip and $image;
