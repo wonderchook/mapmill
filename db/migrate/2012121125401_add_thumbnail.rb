8465cd5e6b303f65a04f552d0bdf03809e22cf4c
Schuyler Erle <schuyler@nocat.net>
2013-03-05 06:18:39 +0000

    Remove broken migration.

--- db/migrate/2012121125401_add_thumbnail.rb
+++
@@ -1,9 +0,0 @@
-class AddThumbnail < ActiveRecord::Migration
-  def self.up
-    add_column :images, :thumbnail, :string, :default => ""
-  end
-
-  def self.down
-    remove_column :images, :thumbnail
-  end
-end
