42e48387e9ba5e60026bd3f7615943e5e94c83b8
Schuyler Erle <schuyler@nocat.net>
2013-05-21 11:53:16 -0700

    make_static_grid a little more flexible

--- util/make_static_grid.sh
+++ util/make_static_grid.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SITE=${1:-sandy}
-echo $SITE
-cd /mnt/mapmill/public/grid/static
-curl http://sandy.hotosm.org/grid/${SITE}.json -o ${SITE}.json
+STATIC=`dirname $0`/../public/grid/static
+mkdir -p ${STATIC}
+cd ${STATIC}
+curl http://localhost/grid/${SITE}.json -o ${SITE}.json
 ogr2ogr ${SITE}_hotosm_damage.shp ${SITE}.json
 zip -9 --move ${SITE}_hotosm_damage.zip ${SITE}_hotosm_damage.*
